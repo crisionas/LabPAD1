015d38d9988ebd57269ab12fb2a2c806759ade83
crisionas <cris.ionas@gmail.com>
2020-10-02 18:21:18 +0300

    Socket Implementation

---
+++ SenderTransaction/BusinessLayer/Common.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sender
+{
+    public class Common
+    {
+
+        public static string ipAddress = "192.168.1.104";
+
+        public static int port = 540;
+    }
+}
